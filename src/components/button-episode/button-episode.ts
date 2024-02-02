b354eb5bb7e893d8277234c6b498c8ebb4bc8328
dustin1818 <dustinamoda18@gmail.com>
2024-02-02 21:02:26 +0800

    add button-episode component

--- src/components/button-episode/button-episode.ts
+++ src/components/button-episode/button-episode.ts
@@ -1,5 +1,32 @@
-import { ICustomElementViewModel } from "aurelia";
+import { IRouteableComponent, IRouter } from "@aurelia/router";
+import { ApiService } from "../../services/api-service";
+import { bindable, inject } from "aurelia";
 
-export class ButtonEpisode implements ICustomElementViewModel {
+@inject(ApiService, IRouter)
+export class ButtonEpisode implements IRouteableComponent {
+    public animeEpisodes = [];
+    @bindable public paramId: string = '';
+    public totalEpisodes = 0;
+
+    constructor(private api: ApiService, private router: IRouter) { }
+
+    async binding() {
+        const data = await this.api.getAnimeEpisodes(this.paramId['id'])
+        this.animeEpisodes = data['episodes'];
+        this.totalEpisodes = data['totalEpisodes'];
+        console.log(this.animeEpisodes)
+    }
+
+    public async getEpisodeDetails(episode: number) {
+        console.log(this.animeEpisodes[episode - 1].episodeId);
+        console.log(this.animeEpisodes[episode - 1].title);
+        await this.router.load('episode', {
+            parameters: {
+                id: this.animeEpisodes[episode - 1].episodeId,
+                title: this.animeEpisodes[episode - 1].title
+            }
+        })
+
+    }
 
 }
