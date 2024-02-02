1e5a7e62d2925e621fc908cbc74af9d9a5f01d50
dustin1818 <dustinamoda18@gmail.com>
2024-02-02 11:37:45 +0800

    update router configuration, ignore git env files, update port to 9001

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import { RouterConfiguration } from '@aurelia/router';
 import { MyApp } from './my-app';
 
 Aurelia
-  .register(RouterConfiguration)
-  // To use HTML5 pushState routes, replace previous line with the following
-  // customized router config.
-  // .register(RouterConfiguration.customize({ useUrlFragmentHash: false }))
+  .register(RouterConfiguration.customize({ useUrlFragmentHash: false }))
   .app(MyApp)
   .start();
