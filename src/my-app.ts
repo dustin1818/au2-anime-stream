3d61727404943aecdd57dd58d97959312cf004d2
dustin1818 <dustinamoda18@gmail.com>
2024-02-02 11:37:10 +0800

    add routes

--- src/my-app.ts
+++ src/my-app.ts
@@ -1,2 +1,33 @@
+import { EventAggregator, IDisposable, inject } from "aurelia";
+
+@inject(EventAggregator)
 export class MyApp {
+    public showSearch = false;
+    public searchListener: IDisposable;
+
+    static routes = [
+        {
+            id: 'home',
+            path: ['', 'home'],
+            component: () => import('./routes/home/home')
+        },
+        {
+            id: 'about',
+            path: 'about',
+            component: () => import('./routes/about/about')
+        }
+
+    ];
+
+    constructor(private ea: EventAggregator) { }
+
+    binding(): void {
+        this.searchListener = this.ea.subscribe('search:open', () => {
+            this.showSearch = true;
+        });
+    }
+
+    unbinding(): void {
+        this.searchListener.dispose();
+    }
 }
