3a59a09efbb82122f4cfd62e477e5c620b26c177
dustin1818 <dustinamoda18@gmail.com>
2024-02-02 21:02:53 +0800

    update anime-info styling

--- src/routes/anime-info/anime-info.ts
+++ src/routes/anime-info/anime-info.ts
@@ -7,14 +7,12 @@ import { animeInfoSchema } from "../../interfaces/animeSchemas";
 @inject(ApiService)
 export class AnimeInfo implements IRouteableComponent {
     public anime: animeInfoSchema[] = [];
-
     constructor(private api: ApiService) { }
 
     public async load(parameters: { id: string }): Promise<void> {
         if (parameters.id) {
             const data = await this.api.getAnimeInfo(parameters.id)
             this.anime = data['anime'].info;
-            console.log(this.anime)
         }
     }
 
