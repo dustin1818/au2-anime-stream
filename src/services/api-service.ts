18247a349b54af823c214a0faaa755f848e2f2d9
dustin1818 <dustinamoda18@gmail.com>
2024-02-03 13:39:13 +0800

    update api, add server for get anime server

--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -4,12 +4,11 @@ import { IHttpClient, json } from "@aurelia/fetch-client";
 @inject(IHttpClient, EventAggregator)
 export class ApiService {
     constructor(private http: IHttpClient, private ea: EventAggregator) {
-        const apiLink = "https://aniwatch-api-b74o.onrender.com/anime";
+        const apiLink = "https://consumet-api-v2.vercel.app/anime/gogoanime";
         http.configure((config) => {
             config
                 .withDefaults({ mode: 'cors' })
                 .withBaseUrl(apiLink);
-            console.log(config)
             return config;
         });
     }
@@ -17,19 +16,25 @@ export class ApiService {
 
     //search anime
     public async searchAnime(query: string, page: number = 1): Promise<any[]> {
-        const response = await this.http.get(`/search?q=${query}&page=${page}`);
+        const response = await this.http.get(`/${query}?page=${page}`);
         return response.json();
     }
 
     //get anime info
     public async getAnimeInfo(id: string): Promise<any> {
-        const response = await this.http.get(`/info?id=${id}`);
+        const response = await this.http.get(`/info/${id}`);
+        return response.json();
+    }
+
+    //get anime server
+    public async getAnimeServer(id: string): Promise<any> {
+        const response = await this.http.get(`/servers/${id}`);
         return response.json();
     }
 
     //get anime episodes
-    public async getAnimeEpisodes(id: string): Promise<any> {
-        const response = await this.http.get(`/episodes/${id}`);
+    public async getEpisodeSource(id: string, server: string = "gogocdn"): Promise<any> {
+        const response = await this.http.get(`/watch/${id}?server=${server}`);
         return response.json();
     }
 } 
