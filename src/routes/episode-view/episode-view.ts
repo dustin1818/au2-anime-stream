f24fac78cff9466fb5b8c6911b1e630188245b80
dustin1818 <dustinamoda18@gmail.com>
2024-02-03 13:40:46 +0800

    add episode-view component

--- src/routes/episode-view/episode-view.ts
+++ src/routes/episode-view/episode-view.ts
@@ -1,5 +1,22 @@
 import { IRouteableComponent } from "@aurelia/router";
-
+import { ApiService } from "../../services/api-service";
+import { inject } from "aurelia";
+import { animeStreamSchema } from "../../interfaces/animeSchemas";
+@inject(ApiService)
 export class EpisodeView implements IRouteableComponent {
+    public id: string = '';
+    public streamData: animeStreamSchema[] = [];
 
+    constructor(private api: ApiService) { }
+
+    public async load(parameters: { id: string }): Promise<void> {
+        if (parameters.id) {
+            this.id = parameters.id;
+        }
+    }
+
+    public async binding(): Promise<void> {
+        const dataServer = await this.api.getEpisodeSource(this.id);
+        console.log(dataServer);
+    }
 }
