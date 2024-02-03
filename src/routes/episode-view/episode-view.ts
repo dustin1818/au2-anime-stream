044295c9f9ef02e749a0e61b05e1422e6b5df87c
dustin1818 <dustinamoda18@gmail.com>
2024-02-03 15:55:55 +0800

    add episode view package

--- src/routes/episode-view/episode-view.ts
+++ src/routes/episode-view/episode-view.ts
@@ -1,13 +1,20 @@
+
 import { IRouteableComponent } from "@aurelia/router";
 import { ApiService } from "../../services/api-service";
-import { inject } from "aurelia";
+import { bindable, inject } from "aurelia";
 import { animeStreamSchema } from "../../interfaces/animeSchemas";
-@inject(ApiService)
+import Hls from 'hls.js';
+
+@inject(ApiService, Hls)
 export class EpisodeView implements IRouteableComponent {
     public id: string = '';
     public streamData: animeStreamSchema[] = [];
+    public quality: string[] = [];
+    public selectedQuality: string = '';
 
-    constructor(private api: ApiService) { }
+    constructor(private api: ApiService, private video: Hls) {
+        console.log(this.selectedQuality)
+    }
 
     public async load(parameters: { id: string }): Promise<void> {
         if (parameters.id) {
@@ -17,6 +24,29 @@ export class EpisodeView implements IRouteableComponent {
 
     public async binding(): Promise<void> {
         const dataServer = await this.api.getEpisodeSource(this.id);
-        console.log(dataServer);
+        this.streamData = dataServer;
+        console.log(this.streamData);
+        this.streamData['sources'].map((source) => {
+            this.quality.push(source.quality);
+        });
     }
+
+    public attached() {
+        const videoPlayer = document.getElementById('videoPlayer') as HTMLVideoElement;
+        this.video.loadSource(this.streamData['sources'][0].url);
+        this.video.attachMedia(videoPlayer);
+    }
+
+    public select() {
+        console.log(this.selectedQuality)
+        this.streamData['sources'].map((source) => {
+            if (source.quality === this.selectedQuality) {
+                const videoPlayer = document.getElementById('videoPlayer') as HTMLVideoElement;
+                const url = source.url;
+                this.video.loadSource(url);
+                this.video.attachMedia(videoPlayer);
+            }
+        });
+    }
+
 }
